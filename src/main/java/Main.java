b30e8e0b981147330e95e89e8369205d4f2d62aa
Caitie Hall <cahall2@uh.edu>
2019-11-02 13:02:34 -0500

    Setting up files for assignment 5

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    //read in JSON file containing info about papers, give program a keyword to search for an an integer, n
+
+    //program will first search for articles w/ titles containing keyword, then finds the papers these articles cited. These are tier-1 papers
+
+    //Tier-2 papers are the ones that were cited by tier-1 papers
+
+    //Output should be info of papers in all tiers up to level n ranked in order of importance.
+        //you decide how importance is defined.
+
+}
